77bf25d60249dbfdd2a125a7bfc57f400aae00d5
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-06-06 22:56:40 +0530

    Building the components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import "./app.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <section id="section-1"></section>
+    </div>
+  );
 }
 
 export default App;
