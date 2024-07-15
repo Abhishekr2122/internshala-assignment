aaa135f7109f1ae58daa142e7114769c51560dd1
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-07-15 10:33:45 +0530

    Refactored the  Product component code.

--- src/ui/Product.js
+++ src/ui/Product.js
@@ -1,28 +1,91 @@
 import "./product.css";
 
-import { FaHouse } from "react-icons/fa6";
+import { IoHome } from "react-icons/io5";
+import { GoWorkflow } from "react-icons/go";
+import { IoIosSearch } from "react-icons/io";
+import { FaBoxOpen } from "react-icons/fa";
+import { PiNetwork } from "react-icons/pi";
+import { GrSettingsOption } from "react-icons/gr";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { TfiThought } from "react-icons/tfi";
 
 export default function Product() {
   return (
     <div className="product">
-      <div className="product-main-container">
+      <div className="product-content-container">
         <div className="product-header-content">
-          <div className="header-content">
-            <FaHouse style={{ color: "orangered" }} />
-            <h4>Product Home</h4>
-          </div>
+          <IoHome />
+          <h5>Home Product</h5>
         </div>
         <div className="product-main-content">
-          <div className="main-content1">
-            <div className="main-card"></div>
-            <div className="main-card"></div>
-            <div className="main-card"></div>
-            <div className="main-card"></div>
+          <div className="product-main-card">
+            <GoWorkflow />
+            <div>
+              <h5 className="product-main-card-heading">
+                HELIX: Work Flow Automation
+              </h5>
+
+              <p className="product-main-card-para">
+                Build and automate custom S&OP Workflow
+              </p>
+            </div>
           </div>
-          <div className="main-content2">
-            <div className="main-card"></div>
-            <div className="main-card"></div>
-            <div className="main-card"></div>
+          <div className="product-main-card">
+            <IoIosSearch />
+            <div>
+              <h5 className="product-main-card-heading">
+                Accurate demand forecasting
+              </h5>
+              <p className="product-main-card-para">
+                Dynamically predict future demand unmatched accuracy
+              </p>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <FaBoxOpen />
+            <div>
+              <h5 className="product-main-card-heading">
+                Automated purchase planning
+              </h5>
+              <p className="product-main-card-para">
+                Automate your purchase order generation
+              </p>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <PiNetwork />
+            <div>
+              <h5 className="product-main-card-heading">
+                Automated Distribution planning
+              </h5>
+              <p className="product-main-card-para">
+                Automate distribution planning and allocation to maintain high
+                fill rates
+              </p>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <GrSettingsOption />
+            <div>
+              <h5 className="product-main-card-heading">Integrations</h5>
+              <p className="product-main-card-para">
+                Yes, Crest works with your tech and ops stack!
+              </p>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <LuLayoutDashboard />
+            <div>
+              <h5 className="product-main-card-heading">Custom dashboards</h5>
+              <p>Build and visualise data your way</p>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <TfiThought />
+            <div>
+              <h5 className="product-main-card-heading">Consensus planning</h5>
+              <p>Collaborate and take better decisions faster</p>
+            </div>
           </div>
         </div>
       </div>
